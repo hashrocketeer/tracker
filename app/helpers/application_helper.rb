0d3fef0b65e63e02368637bc570a783add760810
Johnny Winn <dev+nurugger07@hashrocket.com>
2014-03-03 22:50:58 -0500

    Highlight active navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+
+  def active_home
+    active_matcher URI.decode(request.fullpath) == "/"
+  end
+
+  def active_if(url)
+    active_matcher URI.decode(request.fullpath).match(url)
+  end
+
+  def active_matcher(matcher)
+    matcher ? "active" : ""
+  end
 end
